10a2f0d627a0bbbed94e98297e6eac17bd01645a
kosyakskok <kosyakskok@gmail.com>
2026-04-03 06:46:30 +0300

    send.js

--- api/send.js
+++ api/send.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  // --- CORS ---
+  res.setHeader("Access-Control-Allow-Origin", "https://socketkrasnodar.ru");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  // -------------
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
